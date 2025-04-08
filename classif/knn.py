272f8a6789e3d6c9154e2a622d9e163bc2b03a18
duong <vuhoang.thduong@gmail.com>
2025-04-08 14:19:20 +0200

    add knn and perceptron

---
+++ classif/knn.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+
+from base import Classifier
+
+class ClassifierKNN(Classifier):
+    """ Classe pour représenter un classifieur par K plus proches voisins.
+        Cette classe hérite de la classe Classifier
+    """
+
+    def __init__(self, input_dimension, k):
+        """ Constructeur de ClassifierKNN
+            Argument:
+                - input_dimension (int) : dimension d'entrée des exemples
+                - k (int) : nombre de voisins à considérer
+            Hypothèse : input_dimension > 0
+        """
+        super().__init__(input_dimension)
+        self.k = k
+
+    def score(self, x):
+        """ Rend la proportion des labels parmi les k ppv de x (valeur réelle)
+            x: une description : un ndarray
+        """
+        distances = np.sqrt(np.sum((self.desc_set - x) ** 2, axis=1))
+        nearest_indices = np.argsort(distances)[:self.k]
+        nearest_labels = self.label_set[nearest_indices]
+        label_counts = np.bincount(nearest_labels, minlength=10)
+        return np.argmax(label_counts)
+
+    def predict(self, x):
+        """ Rend la prédiction sur x (label de 0 à 9)
+            x: une description : un ndarray
+        """
+        return self.score(x)
+
+    def train(self, desc_set, label_set):
+        """ Permet d'entraîner le modèle sur l'ensemble donné
+            desc_set: ndarray avec des descriptions
+            label_set: ndarray avec les labels correspondants
+            Hypothèse: desc_set et label_set ont le même nombre de lignes
+        """
+        self.desc_set = desc_set
+        self.label_set = label_set
---
+++ classif/perceptron.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pandas as pd
+
+from base import Classifier
+
+class ClassifierPerceptron(Classifier):
+    """ Perceptron de Rosenblatt
+    """
+    def __init__(self, input_dimension, num_labels=10, learning_rate=0.01, init=True ):
+        """ Constructeur de Classifier
+            Argument:
+                - input_dimension (int) : dimension de la description des exemples (>0)
+                - num_labels (int): nombre de classes dans la classification
+                - learning_rate (par défaut 0.01): epsilon
+                - init est le mode d'initialisation de w: 
+                    - si True (par défaut): initialisation à 0 de w,
+                    - si False : initialisation par tirage aléatoire de valeurs petites
+        """
+        super().__init__(input_dimension)
+        self.num_labels = num_labels
+        self.learning_rate = learning_rate
+
+        # --- Initialisation de w ---
+        self.w = np.zeros((num_labels, input_dimension))  # Each label gets its own weight vector
+        if not init:
+            for i in range(num_labels):
+                for j in range(input_dimension):
+                    self.w[i, j] = ((2*np.random.random()-1) * 0.001)
+
+        self.allw = [self.w.copy()]  # stockage des premiers poids
+
+    def train_step(self, desc_set, label_set):
+        """ Réalise une unique itération sur tous les exemples du dataset
+            donné en prenant les exemples aléatoirement.
+            Arguments:
+                - desc_set: ndarray avec des descriptions
+                - label_set: ndarray avec les labels correspondants
+        """   
+        # Choisir aléatoirement un exemple x_i de X
+        data = list(zip(desc_set, label_set))
+        np.random.shuffle(data)
+
+        for x_i, y_i in data:
+            predict = self.predict(x_i)
+            if y_i != predict:
+                self.w[y_i] += self.learning_rate * x_i
+                self.w[predict] -= self.learning_rate * x_i
+
+            self.allw.append(self.w.copy())
+        
+        return self.w 
+
+    def score(self, x):
+        """ rend le score de prédiction sur x (un vecteur de scores pour chaque classe)
+            x: une description
+        """
+        return np.dot(self.w, x)
+
+    def predict(self, x):
+        """ rend la prediction sur x (un chiffre de 0 à 9)
+            x: une description
+        """
+        scores = self.score(x)
+        return np.argmax(scores)
+
+    def get_allw(self):
+        return self.allw
+
+    def train(self, desc_set, label_set, nb_max=100, seuil=0.001):
+        """ Apprentissage itératif du perceptron sur le dataset donné.
+            Arguments:
+                - desc_set: ndarray avec des descriptions
+                - label_set: ndarray avec les labels correspondants
+                - nb_max (par défaut: 100) : nombre d'itérations maximale
+                - seuil (par défaut: 0.001) : seuil de convergence
+            Retour: la fonction rend une liste
+                - liste des valeurs de norme de différences
+        """  
+        norm_diff_values = []      
+        i = 0
+        
+        while i < nb_max: 
+            w_old = self.w.copy()
+            self.w = self.train_step(desc_set, label_set)
+            
+            norm_diff = np.linalg.norm(w_old - self.w)
+            norm_diff_values.append(norm_diff)
+            
+            if norm_diff < seuil:
+                break
+            
+            i += 1
+        
+        return norm_diff_values
